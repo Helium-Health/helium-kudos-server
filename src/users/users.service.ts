2e6f24578b3584c8ca951f1317a33fd1e86715e9
Kerry <kerryokpere@gmail.com>
2024-09-16 17:07:30 +0100

    fix: use user module

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -8,9 +8,33 @@ import { CreateUserDto } from './dto/User.dto';
 export class UsersService {
   constructor(@InjectModel(User.name) private userModel: Model<User>) {}
 
-  createUser(createUserDto: CreateUserDto) {
+  // Method to create a new user
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
     const newUser = new this.userModel(createUserDto);
 
     return newUser.save();
+  }
+
+  // Method to find a user by email
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+
+  // Additional methods for other user operations
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async updateUser(
+    id: string,
+    updateData: Partial<User>,
+  ): Promise<User | null> {
+    return this.userModel
+      .findByIdAndUpdate(id, updateData, { new: true })
+      .exec();
+  }
+
+  async deleteUser(id: string): Promise<User | null> {
+    return this.userModel.findByIdAndDelete(id).exec();
   }
 }
