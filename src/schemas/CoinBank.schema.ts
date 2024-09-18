f8744ff41c3dadd89a466775bd84292f8224de44
int3ll3kt <int3ll3kt@gmail.com>
2024-09-18 16:31:38 +0100

    WIP

---
+++ src/schemas/CoinBank.schema.ts
@@ -0,0 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type CoinbankDocument = Document & Coinbank;
+
+@Schema({ timestamps: true })
+export class Coinbank {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  userId: Types.ObjectId; // Reference to User entity
+
+  @Prop({ type: Number, required: true, default: 0 })
+  earnedCoins: number;
+
+  @Prop({ type: Number, required: true, default: 0 })
+  coinsAvailable: number;
+}
+
+export const CoinbankSchema = SchemaFactory.createForClass(Coinbank);
---
+++ src/schemas/Milestone.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Types } from 'mongoose';
+
+export type MilestoneDocument = Document & Milestone;
+
+@Schema({ timestamps: true })
+export class Milestone {
+  @Prop({ type: Types.ObjectId, ref: 'User', required: true })
+  userId: Types.ObjectId; // Reference to the User entity
+
+  @Prop({
+    type: String,
+    required: true,
+    enum: ['work anniversary', 'birthday'],
+  })
+  type: string;
+
+  @Prop({ type: String })
+  message: string;
+
+  @Prop({ type: Number, default: 0 })
+  points: number;
+}
+
+export const MilestoneSchema = SchemaFactory.createForClass(Milestone);
