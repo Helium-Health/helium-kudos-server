cd170ccdcdbd84eae7195dc2e70239e0dda80df9
Kerry <kerryokpere@gmail.com>
2024-10-11 14:52:55 +0100

    fix: allow cors on deployed app

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './utils/transform.interceptor';
 import { CurrencySeeder } from './currency/seeds/currency.seed';
-// import * as session from 'express-session';
-// import * as passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,21 +9,9 @@ async function bootstrap() {
   await currencySeeder.seedCurrency();
   app.setGlobalPrefix('api');
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://kudos-staging.onemedtest.com'],
+    credentials: true,
   });
-  // app.use(
-  //   session({
-  //     secret: 'your-secret-key',
-  //     saveUninitialized: false,
-  //     resave: false,
-  //     cookie: {
-  //       maxAge: 60000,
-  //     },
-  //   }),
-  // );
-  // app.use(passport.initialize());
-  // app.use(passport.session());
-
   app.useGlobalInterceptors(new TransformInterceptor());
   await app.listen(3001);
 }
