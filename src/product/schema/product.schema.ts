56937e344f1f29ef50347fb1a8dc41287dd6a79d
int3ll3kt <int3ll3kt@gmail.com>
2024-11-07 17:27:23 +0100

    Product Order

--- src/product/schema/product.schema.ts
+++ src/product/schema/product.schema.ts
@@ -1,21 +1,46 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Document } from 'mongoose';
+import { Document, Types } from 'mongoose';
 
 export type ProductDocument = Document & Product;
+
+export interface ProductResponse extends Omit<Product, 'images'> {
+  images: string[];
+}
+
+@Schema({ timestamps: true })
+export class ProductVariant {
+  @Prop({ type: String, required: true })
+  variantType: string;
+
+  @Prop({ type: String, required: true })
+  value: string;
+
+  @Prop({ type: Number, required: true })
+  price: number;
+
+  @Prop({ type: Number, required: true })
+  stock: number;
+}
 
 @Schema({ timestamps: true })
 export class Product {
   @Prop({ type: String, required: true })
   name: string;
 
-  @Prop({ type: Number, required: true })
-  price: number;
+  @Prop({ type: String })
+  description: string;
 
-  @Prop({ type: String, required: true })
-  category: string;
+  @Prop([String])
+  images: string[];
+
+  @Prop({ type: [{ type: Types.ObjectId, ref: 'Category' }] })
+  categories: Types.ObjectId[];
+
+  @Prop({ type: [ProductVariant], default: [] })
+  variants: ProductVariant[];
 
   @Prop({ type: Number, required: true })
-  quantity: number;
+  basePrice: number;
 }
 
 export const ProductSchema = SchemaFactory.createForClass(Product);
