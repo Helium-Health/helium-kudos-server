fe809918729a1e9ea62e25442a0a3ec3191b54dc
AbdulAkeem <int3ll3kt@gmail.com>
2024-11-20 11:41:15 +0100

    WIP

--- src/recognition/recognition.gateway.ts
+++ src/recognition/recognition.gateway.ts
@@ -7,6 +7,7 @@ import {
   OnGatewayDisconnect,
 } from '@nestjs/websockets';
 import { Socket, Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
 
 @WebSocketGateway({
   cors: {
@@ -17,30 +18,33 @@ export class RecognitionGateway
   implements OnGatewayConnection, OnGatewayDisconnect
 {
   private server: Server;
+  private readonly logger = new Logger(RecognitionGateway.name);
 
-  // Called when a user connects
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    this.logger.log(`Client connected: ${client.id}`);
   }
 
-  // Called when a user disconnects
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+    this.logger.log(`Client disconnected: ${client.id}`);
   }
 
-  // Broadcast a message to all connected clients
   notifyClients() {
-    this.server.emit('recognition-created', {
-      message: 'New recognition posted! Please refresh your page.',
-    });
+    if (this.server) {
+      this.server.emit('recognition-created', {
+        message: 'New recognition posted! Please refresh your page.',
+      });
+    }
   }
 
-  @SubscribeMessage('send-recognition') // This can be used to listen for client requests, if needed
+  @SubscribeMessage('send-recognition')
   handleMessage(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
-    console.log(data);
+    if (!data || !data.recognition) {
+      client.emit('error', 'Invalid recognition data');
+      return;
+    }
+    // Process the valid data here
   }
 
-  // You can inject the server object in the after initialization method
   afterInit(server: Server) {
     this.server = server;
   }
