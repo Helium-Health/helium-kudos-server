aec4356bdb9c866c41cf81e186c9a8b7a5ac0138
Kerry <kerryokpere@gmail.com>
2025-01-07 17:17:00 +0100

    fix: allow all origin

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,12 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors({
-    origin: [
-      'http://localhost:3000',
-      'https://kudos-staging.onemedtest.com',
-      'https://kudos.onemedicalfile.com',
-    ],
+    // origin: [
+    //   'http://localhost:3000',
+    //   'https://kudos-staging.onemedtest.com',
+    //   'https://kudos.onemedicalfile.com',
+    // ],
+    origin: '*',
     credentials: true,
   });
   app.useGlobalInterceptors(new TransformInterceptor());
