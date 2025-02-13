20ff28e144686fba9ee5200dcca6c760dc643278
Kerry <kerryokpere@gmail.com>
2025-02-14 00:18:39 +0100

    fix: add the auth domain for login

--- src/auth/utils/auth0.strategy.ts
+++ src/auth/utils/auth0.strategy.ts
@@ -3,11 +3,11 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
 // TODO AUTH0 STRATEGY HARD Code till 14th Feb 
-const STAGING_AUTH0_DOMAIN="dev-jzt0sqgp553oh8l0.us.auth0.com"
+export const STAGING_AUTH0_DOMAIN="dev-jzt0sqgp553oh8l0.us.auth0.com"
 const STAGING_AUTH0_AUDIENCE="https://dev-jzt0sqgp553oh8l0.us.auth0.com/api/v2/"
 const STAGING_AUTH0_CLIENT_SECRET='pZooTyhyezV4Zmn8SGsAEl1QQnl_ZEbec_NJ5kBSRpPjBU99UkiM4rPHPmPMesoT'
 
-const PROD_AUTH0_DOMAIN="dev-jzt0sqgp553oh8l0.us.auth0.com"
+export const PROD_AUTH0_DOMAIN="dev-jzt0sqgp553oh8l0.us.auth0.com"
 const PROD_AUTH0_AUDIENCE="https://dev-jzt0sqgp553oh8l0.us.auth0.com/api/v2/"
 const PROD_AUTH0_CLIENT_SECRET='EYs5cXdbnkvwFzNCNSG9YaCUdE3ki8mFoI60ftCSPSRvDeHa4gS6P9Y3Ek29vvfK'
 
