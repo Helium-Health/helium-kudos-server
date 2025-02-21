c9c5706d8578f1bf7a43bc6b3dfe310f5b998663
AbdulAkeem <int3ll3kt@gmail.com>
2025-02-21 22:04:43 +0100

    Revert Changes

--- src/milestone/milestone.cron.service.ts
+++ src/milestone/milestone.cron.service.ts
@@ -14,8 +14,7 @@ export class MilestoneCronService {
     private readonly milestoneService: MilestoneService,
   ) {}
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
-  // @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleBirthdayRecognitions() {
     const birthdayMilestone = await this.milestoneService.findByType(
       MilestoneType.BIRTHDAY,
@@ -37,8 +36,7 @@ export class MilestoneCronService {
     }
   }
 
-  @Cron(CronExpression.EVERY_5_MINUTES)
-  // @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
+  @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async handleWorkAnniversaryRecognitions() {
     const anniversaryMilestone = await this.milestoneService.findByType(
       MilestoneType.WORK_ANNIVERSARY,
@@ -59,7 +57,7 @@ export class MilestoneCronService {
     }
   }
 
-  // @Cron('0 0 19 11 *') // Midnight on November 19 (International Men's Day)
+  @Cron('0 0 19 11 *') // Midnight on November 19 (International Men's Day)
   async handleMensDayRecognitions() {
     const mensDayMilestone = await this.milestoneService.findByType(
       MilestoneType.INTERNATIONAL_MENS_DAY,
@@ -76,7 +74,7 @@ export class MilestoneCronService {
     }
   }
 
-  // @Cron('0 0 8 3 *') // Midnight on March 8 (International Women's Day)
+  @Cron('0 0 8 3 *') // Midnight on March 8 (International Women's Day)
   async handleWomensDayRecognitions() {
     const womensDayMilestone = await this.milestoneService.findByType(
       MilestoneType.INTERNATIONAL_WOMENS_DAY,
