5cfd293ce2c783379e8dfda5ffa5a00fd5f1dd8d
int3ll3kt <int3ll3kt@gmail.com>
2025-03-05 11:03:03 +0100

    Fix Incorrect reading of  employment date from sheet

--- src/users/user-sync.service.ts
+++ src/users/user-sync.service.ts
@@ -138,12 +138,16 @@ export class UserSyncService {
     if (!dateString) return undefined;
 
     if (typeof dateString === 'number') {
-      // Convert raw numeric date to a JavaScript date
       const convertedDate = new Date((dateString - 25569) * 86400000);
       return convertedDate;
     }
 
-    const parsedDate = new Date(dateString);
-    return isNaN(parsedDate.getTime()) ? undefined : parsedDate;
+    if (isNaN(new Date(dateString).getTime())) return undefined;
+    const currentYear = new Date().getFullYear();
+    const [month, day] = dateString.split(' ');
+    const date = new Date(
+      Date.UTC(currentYear, new Date(`${month} 1`).getMonth(), parseInt(day)),
+    );
+    return date;
   }
 }
