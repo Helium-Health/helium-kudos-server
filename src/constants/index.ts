28448f6cbc4061088f6854c21854edadfc9edca4
AbdulAkeem <int3ll3kt@gmail.com>
2025-04-11 09:46:26 +0100

    Fix: Adjust Cron Execution Time

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,21 @@
-export const PRODUCTION_CLIENT = 'https://kudos.onemedicalfile.com/'
-export const STAGING_CLIENT = 'https://kudos-staging.onemedtest.com/'
-export const fieldsToMerge = ['team', 'gender', 'nationality', 'joinDate', 'dateOfBirth'];
-export const fieldsToRevert = ['team', 'gender', 'nationality', 'joinDate', 'dateOfBirth', 'originalEmail'];
+export const PRODUCTION_CLIENT = 'https://kudos.onemedicalfile.com/';
+export const STAGING_CLIENT = 'https://kudos-staging.onemedtest.com/';
+export const fieldsToMerge = [
+  'team',
+  'gender',
+  'nationality',
+  'joinDate',
+  'dateOfBirth',
+];
+export const fieldsToRevert = [
+  'team',
+  'gender',
+  'nationality',
+  'joinDate',
+  'dateOfBirth',
+  'originalEmail',
+];
+export const CRON_EVERY_DAY_AT_HALF_PAST_MIDNIGHT = '30 0 * * *';
+export const CRON_EVERY_MARCH_8_0020 = '20 0 8 3 *';
+export const CRON_EVERY_YEAR_ON_NOVEMBER_19_AT_0020 = '20 0 19 11 *';
+
